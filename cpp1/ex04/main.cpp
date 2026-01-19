c004d269c3b215648f692de412be7c7bbb10569a
Maxkirit <maximeturgeon@ymail.com>
2026-01-19 14:26:28 +0100

    feat: cpp1/ex04

--- cpp1/ex04/main.cpp
+++ cpp1/ex04/main.cpp
@@ -6,17 +6,49 @@
 /*   By: mturgeon <maxime.p.turgeon@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 09:39:45 by mturgeon          #+#    #+#             */
-/*   Updated: 2026/01/19 09:45:11 by mturgeon         ###   ########.fr       */
+/*   Updated: 2026/01/19 14:25:41 by mturgeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <fstream>
 
 int main(int argc, char **argv)
 {
 	if (argc != 4)
 		return (1);
-	std::ifstream input = ifs(argv[1]);
+
+	std::string	s1(argv[2]);
+	std::string s2(argv[3]);
+    std::ifstream   input(argv[1]);
+    std::string     buffer;
+    std::string     content("");
+    std::size_t idx = 0;
+    std::size_t found;
+	std::string         newName(argv[1]);
+	std::ofstream       output(newName.append(".replace").c_str());
+
+    //read the content of the file into buffer and append it into content
+    while (getline(input, buffer))
+    {
+        content.append(buffer);
+        if (!input.eof())
+            content.push_back('\n');
+    }
+
+    //find s1 in content, erase if found and replace
+
+    found = content.find(s1, idx);
+    while (found != std::string::npos)
+    {
+        content.erase(found, s1.length());
+        content.insert(found, s2);
+        idx += found;
+        found = content.find(s1, idx);
+    }
+
+	output << content << std::endl;
+	return (1);
 }
